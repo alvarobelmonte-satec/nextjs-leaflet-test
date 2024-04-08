9aa4af4054373ad853ed361862aff65ddfad7145
alvaro.belmonte <alvaro.belmonte@satec.es>
2024-04-08 17:32:40 +0200

    dynamic import map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 'use client'
-import { LeafletMap } from '@/components/LeafletMap';
+import dynamic from "next/dynamic";
+
+export const LeafletComponent = dynamic(
+  () => import('@/components/LeafletMap')
+    // this part is needed if your use a named export
+    // you can replace by ".default" when using a default export
+    .then((mod) => mod.LeafletMap),
+  {
+    // This prevents server-side rendering of BrowserComponent
+    ssr: false,
+  }
+);
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Leaflet Map</h1>
-      <LeafletMap />
+      <LeafletComponent />
     </main>
   );
 }
