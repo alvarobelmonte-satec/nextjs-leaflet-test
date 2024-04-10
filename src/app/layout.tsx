407b26c3fec4d830bacb24da1a54c790b9708307
alvaro.belmonte <alvaro.belmonte@satec.es>
2024-04-10 17:14:58 +0200

    add Suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import FormbricksProvider from '@/components/FormBricks';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <FormbricksProvider />
+      <Suspense>
+        <FormbricksProvider />
+      </Suspense>
       <body className={inter.className}>{children}</body>
     </html>
   );
