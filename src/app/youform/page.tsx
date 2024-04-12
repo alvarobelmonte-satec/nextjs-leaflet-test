bf468617eb846aa015a52115c213c23a686a4cfe
alvaro.belmonte <alvaro.belmonte@satec.es>
2024-04-12 13:12:45 +0200

    fix youform

--- src/app/youform/page.tsx
+++ src/app/youform/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const styles = {
   html: { margin: 0, height: '100%', overflow: 'hidden' },
-  iframe: { position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, border: 0 }
+  iframe: { position: 'absolute' as 'absolute', top: 0, right: 0, bottom: 0, left: 0, border: 0 }
 };
 export default function SurveysPage() {
   return (
@@ -13,9 +13,6 @@ export default function SurveysPage() {
         loading="lazy"
         width="100%"
         height="700"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
       ></iframe>
     </main>
   );
