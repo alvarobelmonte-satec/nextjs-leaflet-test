686fde5e37371d46e685c2801b23cf817a1c5189
alvaro.belmonte <alvaro.belmonte@satec.es>
2024-04-12 13:07:14 +0200

    fix tally form

--- src/app/tally/page.tsx
+++ src/app/tally/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 const styles = {
-  html: { margin: 0, height: '100%', overflow: 'hidden' },
-  iframe: { position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, border: 0 }
+  iframe: { position: 'absolute' as 'absolute', top: 0, right: 0, bottom: 0, left: 0 }
 };
 export default function SurveysPage() {
   return (
@@ -12,9 +11,6 @@ export default function SurveysPage() {
         data-tally-src="https://tally.so/r/nP6ogB?transparentBackground=1"
         width="100%"
         height="100%"
-        frameBorder="0"
-        marginHeight="0"
-        marginWidth="0"
         title="Farmers register"
       ></iframe>
     </main>
